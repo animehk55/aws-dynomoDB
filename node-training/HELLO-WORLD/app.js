24ef3f56867f079b58eaccfdce103525ff3c2379
Animesh Kumar <animeshk@webelight.com>
2019-01-09 20:10:54 +0530

    Added comit

---
+++ node-training/HELLO-WORLD/app.js
@@ -0,0 +1,95 @@
+// // console.log("Hello World");
+
+// var x = "Hello";
+// var y = "Animesh";
+
+// // console.log(x + y);
+
+// console.log(`She sells sea shells 
+// on the sea shore`);
+
+// console.log('She sells sea shells \non the sea shore');
+
+// -----------------------
+// var x = "Hello";
+// x = 2;
+// x=false;
+// console.log(x);
+
+// var z;
+
+
+// ---------------
+// const a = 'my constant';
+// a = 'something else';
+// console.log(a);
+// //error : Assignment to constant variable.
+
+// concatinate
+// var x="Ani";
+// var y="Pu";
+// console.log(x + '  ' + y);
+// console.log(x.length);
+// ----------------
+
+
+// arthematic operation
+// var a=20;
+// var b=30;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a%b);
+// c= 5;
+// console.log(a+b+c);
+// console.log(a*b+c);
+// console.log(a+b*c);
+// console.log((a*b)+c);
+// console.log((a+b)*c);
+// console.log(a>b);
+// console.log(a<b);
+
+
+
+// arthematic operation var issue
+// "use strict"
+// var a=20;
+// var b=30;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a%b);
+// c= 5;
+// console.log(a+b+c);
+// console.log(a*b+c);
+// console.log(a+b*c);
+// console.log((a*b)+c);
+// console.log((a+b)*c);
+// //c is not defined
+
+// ----------------
+
+// var p = true;
+// var q= false;
+// console.log(p & q);
+// console.log(p && q);
+
+// --------------------------
+// var student = {};
+// student.firstname = "Riyaz";
+// student.lastname = "Sayddd";
+// student.age = 35;
+// console.log(student);
+
+
+// -----------------
+var student = {
+    firstname: "Riyax",
+    lastname: "Sahid",
+    age: "35"
+};
+console.log(student.firstname);
+console.log(firstname);
+
+// -----------------------
+Contunue from lecture 15
